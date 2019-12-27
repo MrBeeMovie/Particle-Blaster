e5eaab3bf5a04e56c60820c62215bed751462d72
Max Wertheim <minecart123@aol.com>
2019-12-27 11:50:13 -0500

    Wave text and delay now work. Bug with true or false values for wave to be spawned not resetting.

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveManager : MonoBehaviour
 {
@@ -22,24 +23,50 @@ public class WaveManager : MonoBehaviour
         public EnemyInfo[] enemies;
     }
 
-    private enum State { WAITING, SPAWNING};
+    [SerializeField] private float wave_delay = 5f;
+    [SerializeField] private string WAVE_DELAY_TEXT = "Next Wave Starting In ", 
+        WAVE_NUM_TEXT = "Wave:", SECONDS_STRING = " Seconds";
+
+    private enum State { WAITING, SPAWNING };
     // default state is waiting
     private State state = State.WAITING;
-    private int wave_indx = 0;
+    private int wave_indx = 0, wave_num = 1;
+    private float elapsed_time;
 
     public Wave[] waves;
-    public Transform player;    
+    public Transform player;
+    public Text wave_num_text, wave_delay_text;
+
+    private void Start()
+    {
+        // allow first wave to spawn upon starting up game
+        elapsed_time = wave_delay;
+    }
 
     void Update()
     {
-        if(state == State.WAITING)
+        if (state == State.WAITING)
         {
-            // check if wave_indx is valid
-            if (wave_indx >= waves.Length)
-                wave_indx = 0;
-            //spawn wave at spawn index
-            StartCoroutine(SpawnWave(waves[wave_indx++]));
-        }     
+            if(elapsed_time >= wave_delay)
+            {
+                wave_delay_text.gameObject.SetActive(false);
+
+                // check if wave_indx is valid
+                if (wave_indx >= waves.Length)
+                    wave_indx = 0;
+                //spawn wave at spawn index
+                StartCoroutine(SpawnWave(waves[wave_indx++]));
+            }
+            // otherwise add to elapsed time and show countdown
+            else
+            {
+                wave_delay_text.gameObject.SetActive(true);
+
+                // update elapsed time and text
+                elapsed_time += Time.deltaTime;
+                wave_delay_text.text = WAVE_DELAY_TEXT + (int)(wave_delay - elapsed_time) + SECONDS_STRING;
+            }
+        }
     }
 
     private IEnumerator SpawnWave(Wave wave)
@@ -56,7 +83,12 @@ public class WaveManager : MonoBehaviour
         // wait until all enemies have been spawned
         yield return new WaitUntil(() => CheckWaveSpawned(wave));
 
+        // update wave_num
+        wave_num_text.text = WAVE_NUM_TEXT + (++wave_num);
+
+        // set to waiting state and set elapsed time to zero
         state = State.WAITING;
+        elapsed_time = 0;
 
         yield return null;
     }
